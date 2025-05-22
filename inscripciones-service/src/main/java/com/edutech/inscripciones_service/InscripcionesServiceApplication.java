fd9e9f4f9f61c89f19c2b6820c1be4528627c3ed
TwinSphynx <134968744+AxelOsses@users.noreply.github.com>
2025-05-21 20:58:46 -0400

    Se agregran proyectos de los microservicios

---
+++ inscripciones-service/src/main/java/com/edutech/inscripciones_service/InscripcionesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.inscripciones_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscripcionesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscripcionesServiceApplication.class, args);
+	}
+
+}
