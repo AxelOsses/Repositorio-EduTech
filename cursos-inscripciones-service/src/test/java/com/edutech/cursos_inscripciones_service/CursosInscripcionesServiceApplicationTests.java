6ce4c62e09db0e290371bb295706c0a09aa06b1c
TwinSphynx <134968744+AxelOsses@users.noreply.github.com>
2025-05-22 16:22:57 -0400

    Add files via upload

---
+++ cursos-inscripciones-service/src/test/java/com/edutech/cursos_inscripciones_service/CursosInscripcionesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.cursos_inscripciones_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosInscripcionesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
