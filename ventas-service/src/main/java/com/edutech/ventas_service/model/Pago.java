fc1ae8a5c2daaad088f5308064ad704d77089003
Macaco-Mono <ra.rachid@duocuc.cl>
2025-05-26 20:16:11 -0400

    Avance en el micro servicio de ventas

---
+++ ventas-service/src/main/java/com/edutech/ventas_service/model/Pago.java
@@ -0,0 +1,34 @@
+package com.edutech.ventas_service.model;
+
+import java.sql.Date;
+
+//import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "pago")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Pago {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(nullable = false)
+    private Integer monto;
+
+    @Column(nullable = false)
+    private String metodoPago;
+
+    @Column(nullable = false)
+    private Date fechaPago;
+}
