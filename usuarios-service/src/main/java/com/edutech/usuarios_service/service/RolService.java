0bbe825c005e6e5314acedaef1568c4e382c2b07
TheSanty1510 <santiagoadolfo10@gmail.com>
2025-05-27 20:40:20 -0400

    Arreglado usuario

---
+++ usuarios-service/src/main/java/com/edutech/usuarios_service/service/RolService.java
@@ -0,0 +1,36 @@
+package com.edutech.usuarios_service.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edutech.usuarios_service.model.Rol;
+import com.edutech.usuarios_service.repository.RolRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class RolService {
+
+    @Autowired
+    private RolRepository rolRepository;
+
+    public List<Rol> findAll() {
+        return rolRepository.findAll();
+    }
+
+    public Rol findById(UUID id) {
+        return rolRepository.findById(id).orElse(null);
+    }
+
+    public Rol save(Rol rol) {
+        return rolRepository.save(rol);
+    }
+
+    public void deleteById(UUID id) {
+        rolRepository.deleteById(id);
+    }
+}
