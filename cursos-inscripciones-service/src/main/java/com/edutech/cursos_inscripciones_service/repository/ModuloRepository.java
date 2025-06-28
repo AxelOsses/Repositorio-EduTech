7835dbd2d67323c96136bb9ffa18c556d8781bba
TwinSphynx <134968744+AxelOsses@users.noreply.github.com>
2025-06-28 01:04:50 -0400

    Delete cursos-inscripciones-service/src/main/java/com/edutech/cursos_inscripciones_service/repository/ModuloRepository.java

--- cursos-inscripciones-service/src/main/java/com/edutech/cursos_inscripciones_service/repository/ModuloRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edutech.cursos_inscripciones_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.edutech.cursos_inscripciones_service.model.Modulo;
-
-public interface ModuloRepository extends JpaRepository<Modulo, Long> {
-}
