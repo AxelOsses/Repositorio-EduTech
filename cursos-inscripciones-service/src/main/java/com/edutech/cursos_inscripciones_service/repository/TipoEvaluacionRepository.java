a78be4941e11e2a4f4a03c1352b2080db39300a7
TwinSphynx <134968744+AxelOsses@users.noreply.github.com>
2025-06-28 00:11:27 -0400

    Delete cursos-inscripciones-service/src/main/java/com/edutech/cursos_inscripciones_service/repository/TipoEvaluacionRepository.java

--- cursos-inscripciones-service/src/main/java/com/edutech/cursos_inscripciones_service/repository/TipoEvaluacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edutech.cursos_inscripciones_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.edutech.cursos_inscripciones_service.model.TipoEvaluacion;
-
-
-public interface TipoEvaluacionRepository extends JpaRepository<TipoEvaluacion, Long>{
-
-}
