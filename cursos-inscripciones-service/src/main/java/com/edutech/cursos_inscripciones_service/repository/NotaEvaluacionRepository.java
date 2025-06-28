74821fbddac82b0f2198e0cf1a7040ac4cd8d071
TwinSphynx <134968744+AxelOsses@users.noreply.github.com>
2025-06-28 00:11:09 -0400

    Delete cursos-inscripciones-service/src/main/java/com/edutech/cursos_inscripciones_service/repository/NotaEvaluacionRepository.java

--- cursos-inscripciones-service/src/main/java/com/edutech/cursos_inscripciones_service/repository/NotaEvaluacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.edutech.cursos_inscripciones_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.edutech.cursos_inscripciones_service.model.NotaEvaluacion;
-
-public interface NotaEvaluacionRepository extends JpaRepository<NotaEvaluacion, Long> {
-}
